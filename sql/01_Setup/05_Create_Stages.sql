e5b0b68e89e823b1b7eeb8f8bb8070a0d8680709
Manmeet Singh <manmeet.bangalore@gmail.com>
2026-07-14 20:50:10 +0530

    Update 05_Create_Stages.sql

--- sql/01_Setup/05_Create_Stages.sql
+++ sql/01_Setup/05_Create_Stages.sql
@@ -1,1 +1,55 @@
+/***************************************************************************************************
+File Name      : 04_Create_File_Formats.sql
 
+Project        : RideNow Enterprise Data Platform
+
+Author         : Manmeet Singh
+
+Created Date   : July 2026
+
+Description    : Creates Snowflake Stages.
+
+****************************************************************************************************
+Modification History
+
+Version     Date          Author             Description
+-------     ----------    ----------------   ------------------------------------
+1.0         Jul-2026      Manmeet Singh      Initial Version
+
+***************************************************************************************************/
+
+USE ROLE ACCOUNTADMIN;
+
+USE DATABASE RIDENOW_DB;
+
+USE SCHEMA STAGE;
+
+--==================================================================================
+-- Internal Stage
+--==================================================================================
+
+CREATE OR REPLACE STAGE INTERNAL_STAGE
+COMMENT='Internal Stage for development and testing';
+
+--==================================================================================
+-- AWS S3 External Stage (Placeholder)
+-- Replace bucket name while deployment
+--==================================================================================
+
+CREATE OR REPLACE STAGE AWS_STAGE
+URL='s3://your-bucket-name/ridenow/'
+FILE_FORMAT = RIDENOW_DB.FILE_FORMAT.CSV_FORMAT
+COMMENT='AWS S3 External Stage';
+
+--==================================================================================
+-- Landing Stage
+--==================================================================================
+
+CREATE OR REPLACE STAGE LANDING_STAGE
+COMMENT='Landing Stage for incoming source files';
+
+--==================================================================================
+-- Verify
+--==================================================================================
+
+SHOW STAGES;
